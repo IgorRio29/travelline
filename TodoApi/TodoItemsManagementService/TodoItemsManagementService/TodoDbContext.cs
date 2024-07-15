6fcc43378b69233ff4bc8a4b5ebeffbbd6b69a1e
Igor <igor.riabov@gmail.com>
2024-07-15 15:37:24 +0300

    Added UI and API of ToDo app

---
+++ TodoApi/TodoItemsManagementService/TodoItemsManagementService/TodoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoItemsManagementService.Model;
+
+namespace TodoItemsManagementService
+{
+    public class TodoDbContext : DbContext
+    {
+
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+
+        public DbSet<TodoItem> Items => Set<TodoItem>();
+    }
+}
