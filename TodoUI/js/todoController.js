c9943f545846b3b97a71aad5521febb9a05c62e1
Igor <igor.riabov@gmail.com>
2024-07-17 19:04:12 +0300

    Introduced new service to manage todo items + refactoring

--- TodoUI/js/todoController.js
+++ TodoUI/js/todoController.js
@@ -1,8 +1,4 @@
-const SERVER_BASE_URL = "https://localhost:7053";
-const GET_ITEMS_URL = "/todoitems";
-const GET_ITEMS_FULL_URL = SERVER_BASE_URL + GET_ITEMS_URL;
-
-angular.module('todoApp', []).controller('todoController', function ($http, $log) {
+angular.module('todoApp', []).controller('todoController', function ($http, itemManagementService) {
     this.addItemInitiated = false;
     this.editItemIntitated = false;
     this.removeItemInitiated = false;
@@ -17,12 +13,10 @@ angular.module('todoApp', []).controller('todoController', function ($http, $log
         this.fetchTodoItems();
     }
     this.fetchTodoItems = function () {
-        $http.get(GET_ITEMS_FULL_URL).then(function (response) {
-            self.todoItems = response.data;
-            $log.info("The items fetched successfully. Size: " + response.data.length);
-        }, function (response) {
-            $log.error("The items couldn't be fetched because of service issue");
-        })
+        let promise = itemManagementService.getItems();
+        promise.then(function (value) {
+            self.todoItems = value;
+        });
     }
     this.initiateItemAdding = function () {
         this.addItemInitiated = !this.addItemInitiated;
@@ -49,14 +43,9 @@ angular.module('todoApp', []).controller('todoController', function ($http, $log
             return;
         }
         this.addItemInitiated = false;
-        $http.post(GET_ITEMS_FULL_URL, {
-            name: this.itemName,
-            done: false
-        }).then(function (response) {
-            self.todoItems.push(response.data);
-            $log.info("The item '" + response.data.name + "' added successfully");
-        }, function (response) {
-            $log.error("The item '" + self.itemName + "' couldn't be added because of service issue")
+        let promise = itemManagementService.addItem(this.itemName);
+        promise.then(function (value) {
+            self.todoItems.push(value);
         });
     }
     this.confirmItemEditing = function () {
@@ -64,16 +53,11 @@ angular.module('todoApp', []).controller('todoController', function ($http, $log
             return;
         }
         this.editItemIntitated = false;
-        $http.put(GET_ITEMS_FULL_URL + "/" + this.selectedItem.id, {
-            name: this.newName,
-            done: this.selectedItem.done
-        }).then(function (response) {
-            $log.info("The item '" + self.selectedItem.name + "' updated successfully. New name is '" + self.newName + "'");
+        let promise = itemManagementService.updateItem(this.selectedItem, this.newName, this.selectedItem.done);
+        promise.then(function (value) {
             self.selectedItem.name = self.newName;
             self.newName = "";
-        }, function (response) {
-            $log.error("The item '" + self.selectedItem.name + "' couldn't be updated because of service issue")
-        })
+        });
     }
     this.selectItem = function (item) {
         if (item === this.selectedItem) {
@@ -93,33 +77,19 @@ angular.module('todoApp', []).controller('todoController', function ($http, $log
     }
     this.confirmItemRemoval = function () {
         if (this.hasSelectedItem()) {
-            $http.delete(GET_ITEMS_FULL_URL + "/" + this.selectedItem.id).then(function (response) {
-                $log.info("The item '" + self.selectedItem.name + "' removed successfully");
-                self.todoItems.splice(self.todoItems.indexOf(self.selectedItem), 1);
-                self.removeItemInitiated = false;
-                self.selectedItem = null;
-            }, function (response) {
-                $log.error("The item '" + self.selectedItem.name + "' couldn't be deleted because of service issue")
-            })
-
+            itemManagementService.removeItem(this.selectedItem);
+            self.todoItems.splice(self.todoItems.indexOf(self.selectedItem), 1);
+            self.removeItemInitiated = false;
+            self.selectedItem = null;
         }
     }
     this.toggleDoneState = function () {
         if (this.hasSelectedItem()) {
             var doneState = !this.selectedItem.done;
-            $http.put(GET_ITEMS_FULL_URL + "/" + this.selectedItem.id, {
-                name: this.selectedItem.name,
-                done: doneState
-            }).then(function (response) {
+            let promise = itemManagementService.updateItem(this.selectedItem, this.selectedItem.name, doneState);
+            promise.then(function (value) {
                 self.selectedItem.done = doneState;
-                if (doneState) {
-                    $log.info("The item '" + self.selectedItem.name + "' marked as completed successfully");
-                } else {
-                    $log.info("The item '" + self.selectedItem.name + "' marked as NOT completed successfully");
-                }
-            }, function (response) {
-                $log.error("The item '" + self.selectedItem.name + "' couldn't be masked as completed because of service issue")
-            })
+            });
         }
     }
     this.selectedItemIsDone = function () {
---
+++ TodoUI/js/todoItemManagementService.js
@@ -0,0 +1,60 @@
+const SERVER_BASE_URL = "https://localhost:7053";
+const GET_ITEMS_URL = "/todoitems";
+const GET_ITEMS_FULL_URL = SERVER_BASE_URL + GET_ITEMS_URL;
+
+var todoApp = angular.module('todoApp');
+todoApp.factory('itemManagementService', function ($http, $q, $log) {
+    return {
+        getItems: function () {
+            var deferred = $q.defer();
+            $http.get(GET_ITEMS_FULL_URL).then(function success(response) {
+                deferred.resolve(response.data);
+                $log.info("The items fetched successfully. Size: " + response.data.length);
+            }, function error(response) {
+                $log.error("The items couldn't be fetched because of service issue");
+                deferred.reject(response.status);
+            })
+            return deferred.promise;
+        },
+        addItem: function (name) {
+            var deferred = $q.defer();
+            $http.post(GET_ITEMS_FULL_URL, {
+                name: name,
+                done: false
+            }).then(function success(response) {
+                $log.info("The item '" + response.data.name + "' added successfully");
+                deferred.resolve(response.data);
+            }, function error(response) {
+                $log.error("The item '" + self.itemName + "' couldn't be added because of service issue");
+                deferred.reject(response.status);
+            });
+            return deferred.promise;
+        },
+        updateItem: function (item, newName, doneState) {
+            var deferred = $q.defer();
+            $http.put(GET_ITEMS_FULL_URL + "/" + item.id, {
+                name: newName,
+                done: doneState
+            }).then(function success(response) {
+                $log.info("The item '" + item.name + "' updated successfully");
+                deferred.resolve(response.data);
+            }, function error(response) {
+                $log.error("The item '" + item.name + "' couldn't be updated because of service issue")
+                deferred.reject(response.status);
+            })
+            return deferred.promise;
+        },
+        removeItem: function (item) {
+            var deferred = $q.defer();
+            $http.delete(GET_ITEMS_FULL_URL + "/" + item.id)
+                .then(function success(response) {
+                    $log.info("The item '" + item.name + "' removed successfully");
+                    deferred.resolve(response.data);
+                }, function (response) {
+                    $log.error("The item '" + item.name + "' couldn't be deleted because of service issue")
+                    deferred.reject(response.status);
+                })
+            return deferred.promise;
+        }
+    }
+})
